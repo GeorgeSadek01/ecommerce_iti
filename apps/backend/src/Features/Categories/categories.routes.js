992fe47593e606bbc16a2b75c79a96816c9c203d
WafaeyKhaled1 <Wafaeykhaled732@gmail.com>
2026-03-07 22:54:15 +0200

    Apply CRUD operations for Products

--- apps/backend/src/Features/Categories/categories.routes.js
+++ apps/backend/src/Features/Categories/categories.routes.js
@@ -4,22 +4,16 @@
 // import { createCategory } from './categories.controller.js';
 const express = require('express');
 const router = express.Router();
-const {
-    createCategory,
-    getCategories,
-    updateCategory,
-    deleteCategory,
-    getOneCategory
-} = require('./categories.controller.js');
+const categoryController = require('./categories.controller.js');
 const checkCategoryExists = require('./checkCategoryExists.js');
 
 router.route('/')
-    .post(createCategory)
-    .get(getCategories);
+    .post(categoryController.createCategory)
+    .get(categoryController.getCategories);
 
 router.route('/:id')
-    .get(checkCategoryExists, getOneCategory)
-    .patch(checkCategoryExists, updateCategory)
-    .delete(checkCategoryExists, deleteCategory);
+    .get(checkCategoryExists,categoryController.getOneCategory)
+    .patch(checkCategoryExists,categoryController.updateCategory)
+    .delete(checkCategoryExists, categoryController.deleteCategory);
 
 module.exports = router;
--- apps/backend/src/index.js
+++ apps/backend/src/index.js
@@ -3,6 +3,7 @@
 require('dotenv').config();
 const express = require('express');
 const categoryRoutes = require('./Features/Categories/categories.routes.js');
+const productRoutes = require('./Features/Products/products.routes.js');
 const dbConnect = require('./core/db/dbConnect.js');
 
 // Connect to MongoDB
@@ -13,5 +14,7 @@ app.use(express.json());
 
 // Use the router
 app.use("/api/v1/admin/categories",categoryRoutes);
+
+app.use("/api/v1/seller/products", productRoutes);
 
 app.listen(3000, () => console.log('Server running on port 3000'));
